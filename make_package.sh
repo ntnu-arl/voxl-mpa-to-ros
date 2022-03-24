6e99b6cc3eee665cc07f7f24811095b50e5a9b5b
Alex Gardner <alex.gardner@modalai.com>
2022-03-24 14:54:24 -0700

    update scripts

--- make_package.sh
+++ make_package.sh
@@ -18,75 +18,69 @@ set -e # exit on error to prevent bad ipk from being generated
 ################################################################################
 
 USETIMESTAMP=false
-MAKE_DEB=false
+MAKE_DEB=true
 MAKE_IPK=false
 
 print_usage(){
-	echo ""
-	echo " Package the current project into a deb or ipk package."
-	echo " You must run build.sh first to build the binaries"
-	echo ""
-	echo " Usage:"
-	echo ""
-	echo "  ./make_package.sh ipk"
-	echo "        Build an IPK package for 820"
-	echo ""
-	echo "  ./make_package.sh deb"
-	echo "        Build a DEB package for 865"
-	echo ""
-	echo "  ./make_package.sh 865 timestamp"
-	echo "        Build a DEB package with the current timestamp as a"
-	echo "        suffix in both the package name and deb filename."
-	echo "        This is used by CI for development packages."
-	echo ""
-	echo ""
+    echo ""
+    echo " Package the current project into a deb or ipk package."
+    echo " You must run build.sh first to build the binaries"
+    echo " if no arguments are given it builds a deb"
+    echo ""
+    echo " Usage:"
+    echo "  ./make_package.sh"
+    echo "  ./make_package.sh deb"
+    echo "        Build a DEB package"
+    echo ""
+    echo "  ./make_package.sh ipk"
+    echo "        Build an IPK package"
+    echo ""
+    echo "  ./make_package.sh timestamp"
+    echo "  ./make_package.sh deb timestamp"
+    echo "        Build a DEB package with the current timestamp as a"
+    echo "        suffix in both the package name and deb filename."
+    echo "        This is used by CI for development packages."
+    echo ""
+    echo ""
 }
 
 process_argument () {
 
-	if [ "$#" -ne 1 ]; then
-		echo "ERROR process_argument expected 1 argument"
-		exit 1
-	fi
+    if [ "$#" -ne 1 ]; then
+        echo "ERROR process_argument expected 1 argument"
+        exit 1
+    fi
 
-	## convert argument to lower case for robustness
-	arg=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-	case ${arg} in
-		"")
-			#echo "Making Normal Package"
-			;;
-		"-t"|"timestamp"|"--timestamp")
-			echo "using timestamp suffix"
-			USETIMESTAMP=true
-			;;
-		"-d"|"deb"|"debian"|"--deb"|"--debian")
-			echo "using timestamp suffix"
-			MAKE_DEB=true
-			;;
-		"-i"|"ipk"|"opkg"|"--ipk"|"--opkg")
-			echo "using timestamp suffix"
-			MAKE_IPK=true
-			;;
-		*)
-			echo "invalid option"
-			print_usage
-			exit 1
-	esac
+    ## convert argument to lower case for robustness
+    arg=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+    case ${arg} in
+        "")
+            ;;
+        "-t"|"timestamp"|"--timestamp")
+            echo "using timestamp suffix"
+            USETIMESTAMP=true
+            ;;
+        "-d"|"deb"|"debian"|"--deb"|"--debian")
+            MAKE_DEB=true
+            ;;
+        "-i"|"ipk"|"opkg"|"--ipk"|"--opkg")
+            MAKE_IPK=true
+            MAKE_DEB=false
+            ;;
+        *)
+            echo "invalid option"
+            print_usage
+            exit 1
+    esac
 }
 
 
 ## parse all arguments or run wizard
 for var in "$@"
 do
-	process_argument $var
+    process_argument $var
 done
 
-
-if [ $MAKE_DEB == false ] && [ $MAKE_IPK == false ]; then
-	echo "please specify \"deb\" or \"ipk\" argument to specify what package to build"
-	print_usage
-	exit 1
-fi
 
 ################################################################################
 # variables
@@ -94,7 +88,7 @@ fi
 VERSION=$(cat pkg/control/control | grep "Version" | cut -d' ' -f 2)
 PACKAGE=$(cat pkg/control/control | grep "Package" | cut -d' ' -f 2)
 IPK_NAME=${PACKAGE}_${VERSION}.ipk
-DEB_NAME=${PACKAGE}_${VERSION}.deb
+
 
 DATA_DIR=pkg/data
 CONTROL_DIR=pkg/control
@@ -119,6 +113,7 @@ rm -rf $DEB_DIR
 rm -f *.ipk
 rm -f *.deb
 
+
 ################################################################################
 ## copy useful files into data directory
 ################################################################################
@@ -135,30 +130,30 @@ cp -r catkin_ws/install/share/  $DATA_DIR/opt/ros/indigo/
 ################################################################################
 
 if $MAKE_IPK; then
-	echo "starting building IPK package"
+    echo "starting building IPK package"
 
-	## make a folder dedicated to IPK building and make the required version file
-	mkdir $IPK_DIR
-	echo "2.0" > $IPK_DIR/debian-binary
+    ## make a folder dedicated to IPK building and make the required version file
+    mkdir $IPK_DIR
+    echo "2.0" > $IPK_DIR/debian-binary
 
-	## add tar archives of data and control for the IPK package
-	cd $CONTROL_DIR/
-	tar --create --gzip -f ../../$IPK_DIR/control.tar.gz *
-	cd ../../
-	cd $DATA_DIR/
-	tar --create --gzip -f ../../$IPK_DIR/data.tar.gz *
-	cd ../../
+    ## add tar archives of data and control for the IPK package
+    cd $CONTROL_DIR/
+    tar --create --gzip -f ../../$IPK_DIR/control.tar.gz *
+    cd ../../
+    cd $DATA_DIR/
+    tar --create --gzip -f ../../$IPK_DIR/data.tar.gz *
+    cd ../../
 
-	## update version with timestamp if enabled
-	if $USETIMESTAMP; then
-		dts=$(date +"%Y%m%d%H%M")
-		VERSION="${VERSION}_${dts}"
-		IPK_NAME=${PACKAGE}_${VERSION}.ipk
-		echo "new version with timestamp: $VERSION"
-	fi
+    ## update version with timestamp if enabled
+    if $USETIMESTAMP; then
+        dts=$(date +"%Y%m%d%H%M")
+        VERSION="${VERSION}_${dts}"
+        IPK_NAME=${PACKAGE}_${VERSION}.ipk
+        echo "new version with timestamp: $VERSION"
+    fi
 
-	## use ar to make the final .ipk and place it in the repository root
-	ar -r $IPK_NAME $IPK_DIR/control.tar.gz $IPK_DIR/data.tar.gz $IPK_DIR/debian-binary
+    ## use ar to make the final .ipk and place it in the repository root
+    ar -r $IPK_NAME $IPK_DIR/control.tar.gz $IPK_DIR/data.tar.gz $IPK_DIR/debian-binary
 fi
 
 ################################################################################
@@ -166,26 +161,26 @@ fi
 ################################################################################
 
 if $MAKE_DEB; then
-	echo "starting building Debian Package"
+    echo "starting building Debian Package"
 
-	## make a folder dedicated to IPK building and copy the requires debian-binary file in
-	mkdir $DEB_DIR
+    ## make a folder dedicated to IPK building and copy the requires debian-binary file in
+    mkdir $DEB_DIR
 
-	## copy the control stuff in
-	cp -rf $CONTROL_DIR/ $DEB_DIR/DEBIAN
-	cp -rf $DATA_DIR/*   $DEB_DIR
+    ## copy the control stuff in
+    cp -rf $CONTROL_DIR/ $DEB_DIR/DEBIAN
+    cp -rf $DATA_DIR/*   $DEB_DIR
 
-	## update version with timestamp if enabled
-	if $USETIMESTAMP; then
-		dts=$(date +"%Y%m%d%H%M")
-		sed -E -i "s/Version.*/&-$dts/" $DEB_DIR/DEBIAN/control
-		VERSION="${VERSION}-${dts}"
-		DEB_NAME=${PACKAGE}_${VERSION}.deb
-		echo "new version with timestamp: $VERSION"
-	fi
+    ## update version with timestamp if enabled
+    if $USETIMESTAMP; then
+        dts=$(date +"%Y%m%d%H%M")
+        sed -E -i "s/Version.*/&-$dts/" $DEB_DIR/DEBIAN/control
+        VERSION="${VERSION}-${dts}"
+        echo "new version with timestamp: $VERSION"
+    fi
 
+    DEB_NAME=${PACKAGE}_${VERSION}_arm64.deb
+    dpkg-deb --build ${DEB_DIR} ${DEB_NAME}
 
-	dpkg-deb --build ${DEB_DIR} ${DEB_NAME}
 fi
 
 echo "DONE"
